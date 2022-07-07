46f8e6af575a5501794edc783ff0e2631aa55d13
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-07 20:47:44 +0300

    Create siparis.js

---
+++ models/siparis.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+var siparisSchema = ({
+kullanici:{
+type:String,
+require:true,
+},
+adres:{
+type:String,
+require:true
+},
+urun:{
+type:String,
+require:true,
+},
+urunId:{
+type:String,
+require:true,
+},
+marketId:{
+type:String,
+require:true,
+}
+})
+
+var siparis = mongoose.model('Siparisler', siparisSchema)
+module.exports = siparis
