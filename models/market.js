10be7ae7f22ffb297c990b6aeb0425ce6eeeacb8
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-07 20:47:17 +0300

    Create market.js

---
+++ models/market.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+var yorumlarSchema = new Schema ({
+isim:{
+type:String,
+require:true,
+},
+logo:{
+type:String,
+require:true,
+}
+})
+
+var yorumlar = mongoose.model('Marketler', yorumlarSchema)
+module.exports = yorumlar
