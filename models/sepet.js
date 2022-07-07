6de5a2976b1f7f63cd75154cc802ed372897fe47
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-07 20:47:30 +0300

    Create sepet.js

---
+++ models/sepet.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+var siparisSchema = ({
+kullaniciId:{
+type:String,
+require:true,
+},
+fiyat:{
+type:Number,
+require:true
+},
+urun:{
+type:String,
+require:true,
+},
+urunId:{
+type:String,
+require:true,
+},
+marketId:{
+type:String,
+require:true,
+}
+})
+
+var siparis = mongoose.model('Sepet', siparisSchema)
+module.exports = siparis
